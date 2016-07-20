c4414746255acf8c00e052df1bc8b033820beddf
叶志晟 <yzs981130@126.com>
2016-07-20 22:14:24 +0800

    完全背包

--- testcode/源.cpp
+++ testcode/源.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 int main()
 {
-	int a = 0;
+	//dp[i]为花费为i时最少花费钱数，c[i]为第i种钱的最大个数，ans[i][j]为价格为i时花了j种货币的值
+	//init
+	memset(dp, -1, sizeof(dp));
+	memset(ans, 0, sizeof(num));
+	dp[0] = 0;
+
+	for (int i = 0; i < N; i++) 
+		for (int j = v[i]; j <= V; j++) 
+			if (dp[j - v[i]] != -1 && dp[j - v[i]] + 1 > dp[j]) 
+			{
+				dp[j] = dp[j - v[i]] + 1;
+				for (int k = 0; k < N; k++) 
+				{
+					if (k == i) 
+						ans[j][k] = ans[j - v[i]][k] + 1;
+					else
+						ans[j][k] = ans[j - v[i]][k];
+				}
+			}
 	system("pause");
 	return 0;
 }
